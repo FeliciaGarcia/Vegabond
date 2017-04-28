5cc2f24d4e89b3c2564d11a673ee8dbc6383ca51
ddmilo <ddmilo1029@gmail.com>
2017-04-27 23:20:27 -0400

    Working post edit route and functionality

--- vegabond/app/controllers/posts_controller.rb
+++ vegabond/app/controllers/posts_controller.rb
@@ -10,17 +10,33 @@ class PostsController < ApplicationController
   def new
 
   end
+
   def create
 
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @city = City.find(params[:city_id])
+    @post.update(post_params)
+
+    redirect_to city_path(@city)
+  end
+
+
   def edit
+    @post = Post.find(params[:id])
   end
 
   def delete
   end
 
-
+private
+  def post_params
+    params.require(:post)
+      .permit(:title, :content)
+      .merge(user_id: current_user.id, city_id: params[:city_id])
+  end
 
 
 end
