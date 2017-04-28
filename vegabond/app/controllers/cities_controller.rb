293bc13d1b41f643801eb8830413a7e736a8bf28
lanchanashivananda <lanchanagupta@gmail.com>
2017-04-28 11:28:59 -0400

    Add photo_url for user signup page

--- vegabond/app/controllers/cities_controller.rb
+++ vegabond/app/controllers/cities_controller.rb
@@ -6,6 +6,7 @@ class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
     @posts = @city.posts
+    @posts = @posts.order('updated_at DESC')
   end
 
   def edit
