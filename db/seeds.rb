c18383426d7f7dad7a757b2ad1f15cbead8aca51
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-01 14:20:24 -0400

    add dan milo admin data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 
 
 # user = User.create([{email: 'lanchana@gmail.com', photo_url: "http://i1320.photobucket.com/albums/u528/lanchanagupta/profile-pic_zpsyjza7zrf.jpg", is_admin: true, password: "lanchana", password_confirmation: "lanchana"}])
-user = User.create([{email: 'felicia@gmail.com', photo_url: "http://i.imgur.com/WL327p5.png", is_admin: true, password: "felicia", password_confirmation: "felicia"}])
+# user = User.create([{email: 'felicia@gmail.com', photo_url: "http://i.imgur.com/WL327p5.png", is_admin: true, password: "felicia", password_confirmation: "felicia"}])
+user = User.create([{email: 'danmilo@gmail.com', photo_url: "
+https://scontent-mia1-2.xx.fbcdn.net/v/t1.0-9/14925391_10207979916191933_6732761804125984025_n.jpg?oh=f2f5d07e44b90275b818df26670a4afb&oe=5977D2B8", is_admin: true, password: "danmilo", password_confirmation: "danmilo"}])
